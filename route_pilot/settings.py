fec6de4173140da09768929011c332a4c12d6620
praiseDev12 <senitenttechnology@gmail.com>
2026-06-20 19:10:31 +0100

    update for render deployment

--- route_pilot/settings.py
+++ route_pilot/settings.py
@@ -7,11 +7,21 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-change-me-in-production")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-change-me-in-production"
+)
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
+
+CSRF_TRUSTED_ORIGINS = [
+    os.environ.get("FRONTEND_ORIGIN", "http://localhost:5173")
+]
 
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
@@ -36,19 +46,33 @@ DATABASES = {}
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CORS
-FRONTEND_ORIGIN = os.environ.get("FRONTEND_ORIGIN", "http://localhost:5173")
+
+FRONTEND_ORIGIN = os.environ.get(
+    "FRONTEND_ORIGIN",
+    "http://localhost:5173"
+)
 
 CORS_ALLOWED_ORIGINS = [FRONTEND_ORIGIN]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https?://(localhost|127\.0\.0\.1)(:\d+)?$",
+    r"^https://.*\.vercel\.app$",
 ]
 
 # OSRM
-OSRM_BASE_URL = os.environ.get("OSRM_BASE_URL", "https://router.project-osrm.org")
+
+OSRM_BASE_URL = os.environ.get(
+    "OSRM_BASE_URL",
+    "https://router.project-osrm.org"
+)
 
 # REST Framework
+
 REST_FRAMEWORK = {
-    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
-    "DEFAULT_PARSER_CLASSES": ["rest_framework.parsers.JSONParser"],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer"
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser"
+    ],
 }
